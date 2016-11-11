354efffa7d9b866197a6c5dacd267d7e08208e2a
Rubén Arroyo <rusben@gmail.com>
2016-11-11 11:28:59 +0100

    Film entry form

--- js/control/index.js
+++ js/control/index.js
@@ -10,15 +10,29 @@ $(document).ready(function () {
 
   app.controller("FilmEntryController", function($scope) {
     // Controller properties
-    this.filmType;
+    this.filmType = "Comedy"; // The first selected value in the select
     this.filmsNumber;
+
+    this.filmsName = [];
+    this.directorsName = [];
+    this.prices = [];
+    this.availability = [];
 
     // Scope variables
     $scope.action = 0; // Shows the select and the input
 
     // Methods
     this.generateForm = function () {
-      console.log(this.filmsNumber);
+      $scope.action = 1;
+      $scope.nFields = [this.filmsNumber];
+      // http://stackoverflow.com/questions/16824853/way-to-ng-repeat-defined-number-of-times-instead-of-repeating-over-array
+      
+      // TODO validate this.filmsNumber
+      // We are defining the arrays with a new length
+      this.filmsName = [this.filmsNumber];
+      this.directorsName = [this.filmsNumber];
+      this.prices = [this.filmsNumber];
+      this.availability = [this.filmsNumber];
     };
   });
 
