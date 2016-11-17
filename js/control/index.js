13b2617bf899eeda54c53a8aad057603e936679c
Rubén Arroyo <rusben@gmail.com>
2016-11-17 10:31:24 +0100

    17 Novembre 2016 - 10:30

--- js/control/index.js
+++ js/control/index.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 // angularjs code
 /* (function(){ // write your code here })(); */
 (function(){
+  // This is the instance of our angular app
   var app = angular.module("VideoClubApp", []);
 
   app.controller("FilmEntryController", function($scope) {
@@ -20,19 +21,33 @@ $(document).ready(function () {
 
     // Scope variables
     $scope.action = 0; // Shows the select and the input
+    $scope.nFields = [];
 
     // Methods
     this.generateForm = function () {
       $scope.action = 1;
-      $scope.nFields = [this.filmsNumber];
-      // http://stackoverflow.com/questions/16824853/way-to-ng-repeat-defined-number-of-times-instead-of-repeating-over-array
-      
+
       // TODO validate this.filmsNumber
-      // We are defining the arrays with a new length
-      this.filmsName = [this.filmsNumber];
-      this.directorsName = [this.filmsNumber];
-      this.prices = [this.filmsNumber];
-      this.availability = [this.filmsNumber];
+
+      // Initialize arrays
+      $scope.nFields = [];
+      this.filmsName = [];
+      this.directorsName = [];
+      this.prices = [];
+      this.availability = [];
+
+      // http://stackoverflow.com/questions/16824853/way-to-ng-repeat-defined-number-of-times-instead-of-repeating-over-array
+      for (var i = 0; i < this.filmsNumber; i++) {
+        $scope.nFields.push(i); // 0, 1, 2, 3, 4 ...
+        this.filmsName.push("");
+        this.directorsName.push("");
+        this.prices.push("");
+        this.availability.push("");
+      }
+    };
+
+    this.entryFilms = function () {
+      
     };
   });
 
@@ -44,12 +59,14 @@ $(document).ready(function () {
     'C' - only matches class name
 
 	*/
+  // Normally you use directives to create tags
+  // Driective name must start with lowercase!
 	app.directive("filmEntryForm", function () {
 		return {
 		  restrict: 'E', // type of directive
 		  templateUrl:"templates/film-entry-form.html",
 		  controller: function() {
-
+        // When the document is ready execute this code
 		  },
 		  controllerAs: 'filmEntryFormCtrl' // This is the alias
 		};
